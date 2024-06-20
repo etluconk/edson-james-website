4e3a3dea78a362c2b3ba970c5f9db0842db66750
etluconk <edson@edsonjames.com>
2024-06-20 07:43:30 -0700

    Added hover effect for navigation bar icons

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -20,26 +20,33 @@ const Navigation: React.FC = () => {
 export default Navigation
 
 const NavigationContainer = styled.div`
-    width: calc(100% - 60px);
-    max-width: 680px;
+  width: calc(100% - 60px);
+  max-width: 680px;
 
-    display: flex;
-    justify-content: center;
-    padding-top: 35px;
-    padding-bottom: 25px;
-    border-bottom: solid 3px rgb(var(--accent-color));
-    background-color: var(--page-color);
+  display: flex;
+  justify-content: center;
+  padding-top: 25px;
+  padding-bottom: 15px;
+  border-bottom: solid 3px rgb(var(--accent-color));
+  background-color: var(--page-color);
 
-    position: fixed;
+  position: fixed;
 
-    svg {
-        margin-left: 15px;
-        margin-right: 15px;
-        width: 40px;
-        height: 40px;
+  svg {
+      width: 40px;
+      height: 40px;
+      padding: 20px;
+      margin: 0px;
+  }
+
+  svg path {
+      fill: rgb(var(--accent-color));
+  }
+
+  a:hover svg {
+    background-color: rgb(var(--accent-color));
+    path {
+      fill: var(--page-color);
     }
-
-    svg path {
-        fill: rgb(var(--accent-color));
-    }
+  }
 `
