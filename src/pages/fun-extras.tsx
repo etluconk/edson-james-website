ae39031f70f27e5b9247a3e51a2ea15c9203d646
etluconk <edson@edsonjames.com>
2024-06-19 21:18:17 -0700

    Updated fun extras page

--- src/pages/fun-extras.tsx
+++ src/pages/fun-extras.tsx
@@ -5,9 +5,15 @@ import Layout from "../components/Layout"
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <Container>
         <h1>Fun Extras</h1>
-      </Container>
+        <h2>Goofy little thingies</h2>
+        <p>
+            The following topics aren’t really organized in any particular order; I just didn’t know where else to put them on the website.
+
+            -
+
+            When I was 7 I made a comic called Super Cats featuring feline superheros Flash Cat, Flame Cat, Aqua Cat, Whiskers, and more. The comic’s story consists of multiple extremely underwhelming battles against the villians Lazer Beam, Flame Girl, and the Living Gatorade bottle (this one never quite hit the copying machine). You can read the first 2 of the series right on this website:
+        </p>
     </Layout>
   )
 }
@@ -15,7 +21,3 @@ const IndexPage: React.FC<PageProps> = () => {
 export default IndexPage
 
 export const Head: HeadFC = () => <title>Home Page</title>
-
-const Container = styled.div`
-
-`
