f1ae66eec49ed8df8d26a16cf2097f04d6a61c7d
etluconk <edson@edsonjames.com>
2025-03-24 20:39:43 -0700

    Remove "error" word from 404 error page body text

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,16 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-import Layout from "../components/Layout"
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import Layout from "../components/Layout";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <h1>Ruh roh, this page does not exist buddy</h1>
-      <h2>404 error</h2>
+      <h2>404</h2>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Edson James - 404 Error</title>
+export const Head: HeadFC = () => <title>Edson James - 404 Error</title>;
